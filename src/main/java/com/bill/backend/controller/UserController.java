9acd53ba84cb33a8925c937a2a31ffcd49712ee2
aswin192k5 <bksofficial19@gmail.com>
2025-12-16 23:41:27 +0530

    Fix 404 and 500

--- src/main/java/com/bill/backend/controller/UserController.java
+++ src/main/java/com/bill/backend/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
